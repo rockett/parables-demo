a021b6e446d86196a6dd8ebf8129dcb07f4dfed8
Matthew Lurz <mlurz71@gmail.com>
2009-06-11 20:20:03 -0500

    Moved doctrine-cli and updated to use the resource plugin. Updated the generated sql.

---
+++ scripts/doctrine.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? 
+    getenv('APPLICATION_ENV') : 'development'));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/models'),
+    realpath(APPLICATION_PATH . '/models/generated'),
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+/** Zend_Application */
+require_once 'Zend/Application.php';  
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV, 
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap('doctrine');
+$doctrine = $application->getBootstrap()->getResource('doctrine');
+$cli = new Doctrine_Cli($doctrine['paths']);
+$cli->run($_SERVER['argv']);
