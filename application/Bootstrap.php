10eb0786ae12bd30bdc9d9b20645f108a10f8413
Matthew Lurz <mlurz71@gmail.com>
2009-07-27 11:49:15 -0500

    Implemented include file cache.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,14 +1,4 @@
 <?php
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-    /*
-    public function _initFileIncCache()
-    {
-        $classFileIncCache = APPLICATION_PATH . '/../data/pluginLoaderCache.php';
-        if (file_exists($classFileIncCache)) {
-            include_once $classFileIncCache;
-        }
-        Zend_Loader_PluginLoader::setIncludeFileCache($classFileIncCache);
-    }
-     */
 }
